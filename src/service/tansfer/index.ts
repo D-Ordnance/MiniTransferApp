c36472a3375f64d0cf5e0ac7ec9e34a9a02a11a1
Dolapo <dolapoolakanmi@gmail.com>
2023-11-07 01:21:26 +0100

    env setup and integration

--- src/service/tansfer/index.ts
+++ src/service/tansfer/index.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios'
 import { bankAccount, makeTransferModel, retryTransferModel } from '../../transfer/types/stateTypes';
 import { log } from 'console';
-import { SECRET_KEY } from '../../util/constant';
+import { SECRET_KEY } from '@env';
 
 const BASE_URL = "https://api.flutterwave.com/v3"
 
@@ -10,7 +10,7 @@ axios.defaults.baseURL = BASE_URL;
 axios.defaults.headers.common["Authorization"] = SECRET_KEY
 
 export const getBanks = createAsyncThunk('transfer/banks', async () => {
-    try {        
+    try {
         const resp = await axios.get('banks/NG')
         return resp.data
     } catch (error) {
@@ -19,84 +19,84 @@ export const getBanks = createAsyncThunk('transfer/banks', async () => {
 });
 
 export const checkAccountFieldValidity = createAsyncThunk('transfer/bankAccountValidity', async (data: bankAccount) => {
-    try {        
-        const resp = await axios.post('accounts/resolve', JSON.stringify(data), 
-        {
-            headers: {
-                'Accept': "*/*",
-                'Content-Type': 'application/json'
-            }
-        })
+    try {
+        const resp = await axios.post('accounts/resolve', JSON.stringify(data),
+            {
+                headers: {
+                    'Accept': "*/*",
+                    'Content-Type': 'application/json'
+                }
+            })
         return resp.data
     } catch (error) {
-        if(error.response){
-            if(error.response.data.message !== undefined){
+        if (error.response) {
+            if (error.response.data.message !== undefined) {
                 return error.response.data
             }
         }
-        return {"message": "Something went wrong..."}
+        return { "message": "Something went wrong..." }
     }
 });
 
 export const makeTransferService = createAsyncThunk('transfer/makeTransfer', async (data: makeTransferModel) => {
-    try {        
-        let resp = await axios.post('transfers', JSON.stringify(data), 
-        {
-            headers: {
-                'Accept': "*/*",
-                'Content-Type': 'application/json'
-            }
-        });
+    try {
+        let resp = await axios.post('transfers', JSON.stringify(data),
+            {
+                headers: {
+                    'Accept': "*/*",
+                    'Content-Type': 'application/json'
+                }
+            });
         return resp.data
     } catch (error) {
-        if(error.response){
-            if(error.response.data.message !== undefined){
+        if (error.response) {
+            if (error.response.data.message !== undefined) {
                 return error.response.data
             }
         }
-        return {"message": "Something went wrong..."}
+        return { "message": "Something went wrong..." }
     }
 });
 
 export const confirmTransferService = createAsyncThunk('transfer/confirmTransfer', async (data: retryTransferModel) => {
-    try {        
-        let resp = await axios.post('transactions/'+data.id+"/verify", JSON.stringify(data), 
-        {
-            headers: {
-                'Accept': "*/*",
-                'Content-Type': 'application/json'
-            }
-        });
+    try {
+        let resp = await axios.post('transactions/' + data.id + "/verify", JSON.stringify(data),
+            {
+                headers: {
+                    'Accept': "*/*",
+                    'Content-Type': 'application/json'
+                }
+            });
         return resp.data
     } catch (error) {
         console.log("error is ", error)
-        if(error.response){
+        if (error.response) {
             console.log("status is ", error.response.status);
             console.log("data is ", error.response.data);
-            if(error.response.data.message !== undefined){
+            if (error.response.data.message !== undefined) {
                 return error.response.data
             }
         }
-        return {"message": "Something went wrong..."}
+        return { "message": "Something went wrong..." }
     }
 });
 
 export const retryTransferService = createAsyncThunk('transfer/retryTransfer', async (data: retryTransferModel) => {
     try {
-        let resp = await axios.post('transfers/'+data.id+"/retries", JSON.stringify(data), 
-        {
-            headers: {
-                'Accept': "*/*",
-                'Content-Type': 'application/json'
-            }
-        });
+        let resp = await axios.post('transfers/' + data.id + "/retries", JSON.stringify(data),
+            {
+                headers: {
+                    'Accept': "*/*",
+                    'Content-Type': 'application/json'
+                }
+            });
         return resp.data
     } catch (error) {
-        if(error.response){
-            if(error.response.data.message !== undefined){
+        if (error.response) {
+            if (error.response.data.message !== undefined) {
                 return error.response.data
             }
         }
-        return {"message": "Something went wrong..."}
+        return { "message": "Something went wrong..." }
     }
 });
--- src/util/constant.ts
+++ src/util/constant.ts
@@ -1,3 +1,2 @@
 export const TRANSFER = "@transfer"
 export const BALANCE = "@balance"
-export const SECRET_KEY = `Bearer FLWSECK_TEST-f03005ec866934d8ada9ce81086f1d21-X`
